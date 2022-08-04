5172a25575052a6731ee7c0f341c69a0c090f114
kato9292929 <76525458+kato9292929@users.noreply.github.com>
2022-08-05 08:03:39 +0900

    Initial commit

---
+++ src/components/pages/Top.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Top = () => {
+  return (
+    <SCountainer>
+      <h2>TOPページです</h2>
+    </SCountainer>
+  );
+};
+
+const SCountainer = styled.div`
+  text-align: center;
+`;
